4fab832324e1c26a6554c77f240baa9e191b1dca
Ryan Gibb <ryan@gibbr.org>
2022-09-15 00:57:37 +0200

    refactor complicagted logic into library

--- flake.nix
+++ flake.nix
@@ -1,5 +1,49 @@
 {
-  outputs = {self}: {
-    defaultTemplate.path = ./template;
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    opam-nix.url = "github:RyanGibb/opam-nix";
+    opam-nix.inputs.nixpkgs.follows = "nixpkgs";
+    opam-nix.inputs.flake-utils.follows = "flake-utils";
+
+    opam2json.url = "github:tweag/opam2json";
+    opam2json.inputs.nixpkgs.follows = "nixpkgs";
+    opam-nix.inputs.opam2json.follows = "opam2json";
+
+    # beta so pin commit
+    nix-filter.url = "github:numtide/nix-filter/3e1fff9";
+
+    opam-repository = {
+      url = "github:ocaml/opam-repository";
+      flake = false;
+    };
+    opam-nix.inputs.opam-repository.follows = "opam-repository";
+    opam-overlays = {
+      url = "github:dune-universe/opam-overlays";
+      flake = false;
+    };
+    opam-nix.inputs.opam-overlays.follows = "opam-overlays";
   };
+
+  outputs =
+    { self, nixpkgs, flake-utils, opam-nix, opam2json, nix-filter,
+      opam-repository, opam-overlays, ... }@inputs:
+    {
+      defaultTemplate.path = ./template;
+    } // flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        lib = pkgs.lib;
+        opam-nix = inputs.opam-nix.lib.${system};
+        mirage-nix = import ./src/mirage.nix
+          { inherit pkgs lib flake-utils opam-nix opam2json nix-filter
+          opam-repository opam-overlays; };
+      in {
+        lib = mirage-nix;
+      });
 }
---
+++ src/mirage.nix
@@ -0,0 +1,107 @@
+{ pkgs, lib, flake-utils, opam-nix, opam2json, nix-filter, opam-repository, opam-overlays, ... }:
+
+let
+  inherit (opam-nix) queryToScope buildOpamMonorepo buildOpamProject;
+  inherit (pkgs.lib.attrsets) mapAttrsToList mapAttrs' nameValuePair;
+in rec {
+  # run `mirage configure` on source,
+  # with mirage, dune, and ocaml from `opam-nix`
+  configureSrcFor = unikernelName: mirageDir: target:
+    # Get mirage tool and dependancies from opam.
+    # We could also get them from nixpkgs but they may not be up to date.
+    let configure-scope = queryToScope { } { mirage = "*"; }; in
+    pkgs.stdenv.mkDerivation {
+      name = "configured-src";
+      # only copy these files and only rebuild when they change
+      src = with nix-filter.lib;
+      filter {
+        root = self;
+        exclude = [
+        (inDirectory "_build")
+        (inDirectory "dist")
+        (inDirectory "duniverse")
+        (inDirectory "${mirageDir}/mirage")
+        "dune"
+        "dune.build"
+        "dune.config"
+        "dune-project"
+        "dune-workspace"
+        "Makefile"
+        "flake.nix"
+        "flake.lock"
+        ];
+      };
+      buildInputs = with configure-scope; [ mirage ];
+      nativeBuildInputs = with configure-scope; [ dune ocaml ];
+      phases = [ "unpackPhase" "configurePhase" "installPhase" "fixupPhase" ];
+      configurePhase = ''
+      mirage configure -f ${mirageDir}/config.ml -t ${target}
+      # Rename the opam file for package name consistency
+      # And move to root so a recursive search for opam files isn't required
+      cp ${mirageDir}/mirage/${unikernelName}-${target}.opam ${unikernelName}.opam
+      '';
+      installPhase = "cp -R . $out";
+    };
+
+  # collect all dependancy sources in a scope
+  mkScopeMonorepo = src: buildOpamMonorepo { } src { };
+
+  # read all the opam files from the configured source and build the ${unikernelName} package
+  mkScopeOpam = unikernelName: mirageDir: src:
+    let
+      scope = buildOpamProject { } unikernelName src { };
+      overlay = final: prev: {
+        "${unikernelName}" = prev.${unikernelName}.overrideAttrs (_ :
+          let monorepo-scope = mkScopeMonorepo src; in
+          {
+          phases = [ "unpackPhase" "preBuild" "buildPhase" "installPhase" ];
+          preBuild =
+            let
+            # TODO get dune build to pick up symlinks
+            createDep = name: path: "cp -r ${path} duniverse/${name}";
+            createDeps = mapAttrsToList createDep monorepo-scope;
+            createDuniverse = builtins.concatStringsSep "\n" createDeps;
+            in
+          ''
+            # find solo5 toolchain
+            ${if final ? ocaml-solo5 then
+            "export OCAMLFIND_CONF=\"${final.ocaml-solo5}/lib/findlib.conf\""
+            else ""}
+            # create duniverse
+            mkdir duniverse
+            echo '(vendored_dirs *)' > duniverse/dune
+            ${createDuniverse}
+          '';
+          buildPhase = ''
+            dune build ${mirageDir}
+          '';
+          installPhase = ''
+            mkdir $out
+            cp -L ./dist/${unikernelName}* $out/
+          '';
+          }
+        );
+      };
+    in scope.overrideScope' overlay;
+
+  mkUnikernelPackages = { unikernelName, mirageDir ? "." }: let
+    targets = [ "xen" "qubes" "unix" "macosx" "virtio" "hvt" "spt" "muen" "genode" ];
+    mapTargets = mkScope:
+    let
+      pipeTarget = target: lib.pipe target [
+        (configureSrcFor unikernelName mirageDir)
+        mkScope
+      ];
+      mappedTargets = builtins.map (target: nameValuePair target (pipeTarget target)) targets;
+    in builtins.listToAttrs mappedTargets;
+      targetUnikernels = mapAttrs'
+        (target: scope: nameValuePair target scope.${unikernelName})
+        (mapTargets (mkScopeOpam unikernelName mirageDir));
+      targetScopes = mapAttrs'
+        (target: scope: nameValuePair "${target}-scope" scope)
+        (mapTargets (mkScopeOpam unikernelName mirageDir));
+      targetMonorepoScopes = mapAttrs'
+        (target: scope: nameValuePair "${target}-monorepo" scope)
+        (mapTargets mkScopeMonorepo);
+  in targetUnikernels // targetScopes // targetMonorepoScopes;
+}
